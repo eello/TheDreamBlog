c8ccfb0232977f521681430e18168ebe0f8518a8
jongseong-mac <kjs3870@naver.com>
2021-02-13 20:50:59 +0900

    [BE] Posting CRUD / apply Middleware ***     post.module: production 환경에서 UnAuthMiddleware 적용 ***

--- server/src/post/post.module.ts
+++ server/src/post/post.module.ts
@@ -1,14 +1,29 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { Post } from './post.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { TagRepository } from 'src/tag/tag.repository';
 import { LoggerModule } from 'src/logger/logger.module';
+import { UnAuthMiddleware } from 'src/middleware/un-auth.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post, TagRepository]), LoggerModule],
   controllers: [PostController],
   providers: [PostService],
 })
-export class PostModule {}
+export class PostModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    if (process.env.NODE_ENV === 'production') {
+      consumer
+        .apply(UnAuthMiddleware)
+        .exclude({ path: '/thedream.api/post', method: RequestMethod.GET })
+        .forRoutes('project');
+    }
+  }
+}
